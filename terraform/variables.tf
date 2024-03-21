3eec60aea6f861dac0d4744cfc495c12b57cfd20
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-22 00:27:31 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,45 @@
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "hobbes"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "eastus"
+}
+
+variable "rg-name" {
+  type        = string
+  description = "A prefix used for all resources in this example"
+  default     = "sas-test"
+}
+
+variable "cluster-name" {
+  type        = string
+  description = "A prefix used for all resources in this example"
+  default     = "sas-test-aks"
+}
+
+variable "acr-name" {
+  type        = string
+  description = "A prefix used for all resources in this example"
+  default     = "sasaksacrtest"
+}
+
+variable "aks-version" {
+  type        = string
+  description = "A prefix used for all resources in this example"
+  default     = "1.27.9"
+}
+
+variable "appId" {
+  type        = string
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "password" {
+  type        = string
+  description = "Azure Kubernetes Service Cluster password"
+}
