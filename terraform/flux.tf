366cd08141a28067479303178ee8d8be055ca5e9
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-22 22:34:20 +0530

    Update flux.tf

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -6,7 +6,7 @@ resource "github_repository_deploy_key" "this" {
   title      = "Flux"
   repository = var.github_repository
   key        = tls_private_key.flux.public_key_openssh
-  read_only  = "false"
+  read_only  = "true"
 }
 resource "flux_bootstrap_git" "main" {
   depends_on = [gitlab_deploy_key.main]
