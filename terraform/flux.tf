0da93e8f0780f54575a32ebbaf879d01ec35d9ca
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-22 22:54:42 +0530

    Update flux.tf

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -2,7 +2,7 @@ resource "tls_private_key" "flux" {
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
 }
-resource "github_repository_deploy_key" "this" {
+resource "github_repository_deploy_key" "main" {
   title      = "Flux"
   repository = var.github_repository
   key        = tls_private_key.flux.public_key_openssh
