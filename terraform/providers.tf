4e8e2f75e1beae2edb1ccdebdc33ea34a7604397
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-22 23:28:12 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,7 +15,7 @@ provider "github" {
 }
 provider "flux" {
   kubernetes = {
-    host                   = azurerm_kubernetes_cluster.main.endpoint
+    host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
     client_certificate     = azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
     client_key             = azurerm_kubernetes_cluster.main.kube_config.0.client_key
     cluster_ca_certificate = azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
