a81d3249e6146da791db910f040dbba593d33e11
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-22 22:17:29 +0530

    Update flux.tf

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -3,18 +3,18 @@ resource "tls_private_key" "flux" {
   ecdsa_curve = "P256"
 }
 
-data "gitlab_project" "this" {
+data "gitlab_project" "main" {
   path_with_namespace = "${var.gitlab_group}/${var.gitlab_project}"
 }
 
-resource "gitlab_deploy_key" "master" {
-  project  = data.gitlab_project.this.id
+resource "gitlab_deploy_key" "main" {
+  project  = data.gitlab_project.main.id
   title    = "Flux"
   key      = tls_private_key.flux.public_key_openssh
   can_push = true
 }
 
-resource "flux_bootstrap_git" "this" {
-  depends_on = [gitlab_deploy_key.master]
+resource "flux_bootstrap_git" "main" {
+  depends_on = [gitlab_deploy_key.main]
   path = "clusters/sas-test-aks"
 }
