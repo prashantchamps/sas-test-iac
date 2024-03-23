869d928603130cf2d9fc5648400d8e166c5a324b
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-23 21:16:37 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
 provider "azuread" {}
 provider "helm" {
   kubernetes {
+    load_config_file       = "false"
     host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
@@ -15,6 +16,7 @@ provider "github" {
 }
 provider "flux" {
   kubernetes = {
+    load_config_file       = "false"
     host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
