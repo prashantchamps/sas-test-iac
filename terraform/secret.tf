76e80edbfb95d2315ed67bdb49c2e79dc4a5a58f
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-23 20:37:11 +0530

    Create secret.tf

---
+++ terraform/secret.tf
@@ -0,0 +1,26 @@
+resource "kubernetes_secret" "main" {
+  metadata {
+    name = "regsecret"
+    namespace = "flux-system"
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.registry_server}" = {
+          "username" = var.appId
+          "password" = var.password
+          "auth"     = base64encode("${var.appId}:${var.password}")
+        }
+      }
+    })
+  }
+
+  depends_on = [
+    flux_bootstrap_git.main,
+    azurerm_kubernetes_cluster.main,
+    azurerm_container_registry.main
+  ]
+}
