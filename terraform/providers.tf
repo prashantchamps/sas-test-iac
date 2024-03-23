4f2fce710b2cf9644a7d41ca9a1569b25d3ba991
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-23 22:23:07 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,7 +15,7 @@ provider "github" {
 }
 provider "flux" {
   kubernetes = {
-    host                   = "sas-test-aks-yhr6idti.hcp.eastus.azmk8s.io"#azurerm_kubernetes_cluster.main.kube_config.0.host
+    host                   = "sas-test-aks-yhr6idti.hcp.eastus.azmk8s.io" #azurerm_kubernetes_cluster.main.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
