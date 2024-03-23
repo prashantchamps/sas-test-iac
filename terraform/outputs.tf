7d78d4d4a5edfaf34e09fe5181c1799c96afd7e0
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-23 22:00:58 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,5 @@
+output "admin_host" {
+  description = "The `host` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. The Kubernetes cluster server host."
+  sensitive   = true
+  value       = try(azurerm_kubernetes_cluster.main.kube_admin_config[0].host, "")
+}
