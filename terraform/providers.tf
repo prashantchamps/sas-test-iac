5c54055fa1d5b08e615f932abd177c4607a2a33d
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-24 23:52:49 +0530

    Update providers.tf
    removed commented lines

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -58,7 +58,3 @@ terraform {
   }
 }
 data "azurerm_client_config" "current" {}
-#data "azurerm_kubernetes_cluster" "main" {
-#  name                = azurerm_kubernetes_cluster.main.name
-#  depends_on          = [azurerm_kubernetes_cluster.main]
-#}
