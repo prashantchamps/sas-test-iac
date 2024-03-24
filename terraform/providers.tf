5fdffaac87e6536be996279139f1c49c4f839b11
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-25 03:21:59 +0530

    Update providers.tf
    given kube config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,27 +4,7 @@ provider "azurerm" {
 provider "azuread" {}
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "kubelogin"
-      args = [
-        "get-token",
-        "--environment",
-        "AzurePublicCloud",
-        "--server-id",
-        "6dae42f8-4368-4678-94ff-3960e28e3630", # Note: The AAD server app ID of AKS Managed AAD is always 6dae42f8-4368-4678-94ff-3960e28e3630 in any environments.
-        "--client-id",
-        "0c1f4e62-611a-42f1-8f3d-9c23b62598bf", # SPN App Id created via terraform
-        "--client-secret",
-        "yFK8Q~.XKoA9DcLCcJfd7h3TKT6vtisDLZBDdaNx",
-        "--tenant-id",
-        "d5c750a2-41db-45f0-9332-2e0679e9d40b", # AAD Tenant Id
-        "--login",
-        "spn"
-      ]
-    }
+    config_path = "~/.kube/config"
     #host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
     #client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
     #client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
@@ -36,27 +16,7 @@ provider "github" {
 }
 provider "flux" {
   kubernetes = {
-    host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "kubelogin"
-      args = [
-        "get-token",
-        "--environment",
-        "AzurePublicCloud",
-        "--server-id",
-        "6dae42f8-4368-4678-94ff-3960e28e3630", # Note: The AAD server app ID of AKS Managed AAD is always 6dae42f8-4368-4678-94ff-3960e28e3630 in any environments.
-        "--client-id",
-        "0c1f4e62-611a-42f1-8f3d-9c23b62598bf", # SPN App Id created via terraform
-        "--client-secret",
-        "yFK8Q~.XKoA9DcLCcJfd7h3TKT6vtisDLZBDdaNx",
-        "--tenant-id",
-        "d5c750a2-41db-45f0-9332-2e0679e9d40b", # AAD Tenant Id
-        "--login",
-        "spn"
-      ]
-    }
+    config_path = "~/.kube/config"
     #host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
     #client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
     #client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
