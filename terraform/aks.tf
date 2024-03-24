9a5e8eea87f3f38b2762526fffe719b60170e2e4
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-24 23:51:36 +0530

    Update aks.tf
    removed commented lines

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -76,14 +76,3 @@ resource "azurerm_role_assignment" "acrpull_role" {
     azurerm_container_registry.main
   ]
 }
-
-#resource "azurerm_role_assignment" "acrpull_role" {
-#  scope                = azurerm_container_registry.main.id
-#  role_definition_name = "AcrPull"
-#  principal_id         = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
-#  depends_on = [
-#    azurerm_resource_group.main,
-#    azurerm_kubernetes_cluster.main,
-#    azurerm_container_registry.main
-#  ]
-#}
