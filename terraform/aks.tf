4330d39c52e8510b5fe011e7eebd082d091cadb1
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-24 14:55:59 +0530

    Update aks.tf
    formatted

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,21 +1,21 @@
 resource "azurerm_kubernetes_cluster" "main" {
-  name                      = var.cluster-name
-  location                  = azurerm_resource_group.main.location
-  resource_group_name       = azurerm_resource_group.main.name
-  dns_prefix                = var.cluster-name
-  kubernetes_version        = var.aks-version
-  automatic_channel_upgrade = "stable"
-  private_cluster_enabled   = false
-  sku_tier                  = "Free"
-  oidc_issuer_enabled       = true
-  workload_identity_enabled = true
-
+  name                              = var.cluster-name
+  location                          = azurerm_resource_group.main.location
+  resource_group_name               = azurerm_resource_group.main.name
+  dns_prefix                        = var.cluster-name
+  kubernetes_version                = var.aks-version
+  automatic_channel_upgrade         = "stable"
+  private_cluster_enabled           = false
+  sku_tier                          = "Free"
+  oidc_issuer_enabled               = true
+  workload_identity_enabled         = true
+  role_based_access_control_enabled = true
+  local_account_disabled            = true
   network_profile {
     network_plugin = "azure"
     dns_service_ip = "10.0.64.10"
     service_cidr   = "10.0.64.0/19"
   }
-
   default_node_pool {
     name                 = "default"
     vm_size              = "Standard_D2_v2"
@@ -27,15 +27,10 @@ resource "azurerm_kubernetes_cluster" "main" {
     min_count            = 1
     max_count            = 1
   }
-
   service_principal {
     client_id     = var.appId
     client_secret = var.password
   }
-
-  role_based_access_control_enabled = true
-  local_account_disabled = true
-
   depends_on = [
     azurerm_resource_group.main,
     azurerm_container_registry.main,
@@ -50,12 +45,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "main" {
   vm_size               = "Standard_DS2_v2"
   vnet_subnet_id        = azurerm_subnet.subnet1.id
   orchestrator_version  = var.aks-version
-
-  enable_auto_scaling = true
-  node_count          = 1
-  min_count           = 1
-  max_count           = 1
-
+  enable_auto_scaling   = true
+  node_count            = 1
+  min_count             = 1
+  max_count             = 1
   depends_on = [
     azurerm_resource_group.main,
     azurerm_kubernetes_cluster.main,
