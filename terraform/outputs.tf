934606747024a9a25e036362a6df82150e8e0bb9
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2024-03-25 01:08:27 +0530

    Delete terraform/outputs.tf
    deleted output.tf

--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "admin_host" {
-  description = "The `host` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. The Kubernetes cluster server host."
-  sensitive   = false
-  value       = try(azurerm_kubernetes_cluster.main.kube_admin_config[0].host, "")
-}
